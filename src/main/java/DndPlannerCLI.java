37358ebf01368d02a50a94815fdc497278747741
Samuel Muskovitz <samuel.muskovitz@gmail.com>
2022-06-29 12:17:07 -0400

    Working on integrating Spring into project

--- src/main/java/DndPlannerCLI.java
+++ src/main/java/DndPlannerCLI.java
@@ -1,23 +1,10 @@
 import dao.*;
-import model.Campaign;
-import model.DayOfWeek;
-import model.Person;
-import model.SpecificDay;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@SpringBootApplication
 public class DndPlannerCLI {
-
-    /*ToDo
-    Write interface for CampaignDao
-    Create POJO model for Campaign
-    Refactor DayOfWeekDao
-    Impement JdbcSpecificDayDao
-    Implement JdbcCampaignDao
-     */
-
 
     private final PersonDao personDao;
     private final DayOfWeekDao dayOfWeekDao;
@@ -31,7 +18,7 @@ public class DndPlannerCLI {
         dataSource.setPassword("postgres1");
 
         DndPlannerCLI application = new DndPlannerCLI(dataSource);
-        application.run();
+        //SpringApplication.run(DndPlannerCLI.class, args);
     }
 
     public DndPlannerCLI(BasicDataSource dataSource){
@@ -39,11 +26,6 @@ public class DndPlannerCLI {
         dayOfWeekDao = new JdbcDayOfWeekDao(dataSource);
         specificDayDao = new JdbcSpecificDayDao(dataSource);
         campaignDao = new JdbcCampaignDao(dataSource);
-    }
-
-    public void run(){
-
-
     }
 
 }
